a5b824c5d17dbe9e30518e75dc115f40b71bf0a1
muhammadnadeem <muhammad.nadeem@techoverflow.io>
2023-05-25 23:41:52 +0500

    resolved errors

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css';
-import Sidebar from './components/sidebar';
+import Sidebar from './components/Sidebar';
 import { personalData } from '../data/page-data';
 
 export const metadata = {
